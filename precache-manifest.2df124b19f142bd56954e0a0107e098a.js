0f6ef65b2e3d2f9ba8cc1022e11a18ade8d49456
raad1masum <piraadwan@gmail.com>
2020-06-09 19:09:58 -0700

    Updates

---
+++ precache-manifest.2df124b19f142bd56954e0a0107e098a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f7f9e1ff7a0910ecbaa83f1a56a75f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7567382dbae74edfeb0",
+    "url": "/static/css/main.72d54c75.chunk.css"
+  },
+  {
+    "revision": "b6a8d181b8e01dba0bc0",
+    "url": "/static/js/2.73ecbca6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.73ecbca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7567382dbae74edfeb0",
+    "url": "/static/js/main.dc0480fe.chunk.js"
+  },
+  {
+    "revision": "54f30fcaea9500ce404a",
+    "url": "/static/js/runtime-main.b4c216f6.js"
+  }
+]);
