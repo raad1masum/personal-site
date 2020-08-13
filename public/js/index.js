77ace981597d6e51de01b6ddd49b74735d532b33
raad1masum <piraadwan@gmail.com>
2020-08-13 09:58:42 -0700

    move js out of html

--- public/js/index.js
+++ public/js/index.js
@@ -2,13 +2,24 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+    });
 }
+$((function() {
+    $(document).scroll((function() {
+        var t = $("nav");
+        var tm = $(".navbar-mobile")
+        t.toggleClass("nav-shadow", $(this).scrollTop() > t.height())
+        tm.toggleClass("nav-mobile-shadow", $(this).scrollTop() > t.height())
+    }
+    ))
+}
+))
+
